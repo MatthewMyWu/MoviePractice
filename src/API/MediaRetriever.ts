fa88b5cc8a65009e0873afa2c462a492bb9c18b8
dingdongg <cs.dg.kim@gmail.com>
2022-12-30 17:27:51 -0700

    create mock implementation of API interface

--- src/API/MediaRetriever.ts
+++ src/API/MediaRetriever.ts
@@ -30,8 +30,55 @@ export const enum MediaFilter {
 }
 
 export interface IMediaRetriever {
-    getTrendingMovies(startIndex: number, endINdex: number): IMediaDisplayInfo[];
-    getTrendingTV(startIndex: number, endINdex: number): IMediaDisplayInfo[];
+    getTrendingMovies(startIndex: number, endIndex: number): IMediaDisplayInfo[];
+    getTrendingTV(startIndex: number, endIndex: number): IMediaDisplayInfo[];
     search(searchText: string, filter: FilterOption, mediaFilter: MediaFilter ): IMediaDisplayInfo[];
     getMediaByID(id: number): IMediaFullInfo;
+}
+
+export class MockMediaRetriever implements IMediaRetriever {
+    getTrendingMovies(startIndex: number, endIndex: number): IMediaDisplayInfo[] {
+        const ret: IMediaDisplayInfo[] = [];
+
+        for (let i = 0; i < 5; i++) {
+            ret.push({
+                "title": `title of movie ${i + 1}`,
+                "posterPath": "/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg",
+                "id": i + 1,
+            });
+        }
+
+        return ret;
+    }
+
+    getTrendingTV(startIndex: number, endIndex: number): IMediaDisplayInfo[] {
+        const ret: IMediaDisplayInfo[] = [];
+
+        for (let i = 0; i < 5; i++) {
+            ret.push({
+                "title": `title of TV show ${i + 1}`,
+                "posterPath": "/qWnJzyZhyy74gjpSjIXWmuk0ifX.jpg",
+                "id": i + 1,
+            });
+        }
+
+        return ret;
+    }
+
+    search(searchText: string, filter: FilterOption, mediaFilter: MediaFilter ): IMediaDisplayInfo[] {
+        return [];
+    }
+
+    getMediaByID(id: number): IMediaFullInfo {
+        return {
+            "title": "some media",
+            "overview": "media overview media overview media overview",
+            "backdropPath": "string", // Should be full URL to image
+            "releaseDate": "2022-12-30",
+            "voteAverage": 12345,
+            "genres": [],
+            "cast": [],
+            "directors": []
+        };
+    }
 }
